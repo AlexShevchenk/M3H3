9f6e2fc4128588f9ec77d5f936dc5a458a6100aa
Oleksandr Shevchenko <105636389+AlexShevchenk@users.noreply.github.com>
2023-07-10 18:19:58 +0300

    Add File via HW

---
+++ HW_3_3/Mail/Topics.cs
@@ -0,0 +1,15 @@
+﻿ 
+ 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_3_3.Mail
+{
+    internal enum Topic
+    {
+        technology,
+        fishing,
+        fashion
+    }
+}
